1039aed7a8d31794102aa50a269ca7db97695f28
Sea <xigart1@gmail.com>
2026-01-01 22:20:39 +0400

    refactor(service.py): add constrained types to PredictionData

--- service.py
+++ service.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing_extensions import Annotated
+from datetime import datetime
 
+County = Annotated[int, Field(ge=0, le=15)]
+Product_Type = Annotated[int, Field(ge=0, le=3)]
 
 class PredictionData(BaseModel):
-    county: int
-    installed_capacity: float
+    county: County
+    is_business: bool
+    product_type: Product_Type
+    is_consumption: bool
+    timestamp: datetime
 
 
 app = FastAPI()
@@ -18,5 +25,5 @@ def root():
 
 @app.post("/predict")
 def predict(data: PredictionData):
-    target = data.county * data.installed_capacity
+    target = data.county * data.product_type
     return {"prediction": target}
